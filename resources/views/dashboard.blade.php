7b29ef69d9df522e8e20463824465d6f4f66c269
Nat Zimmermann <nat@razorcreations.com>
2017-03-14 13:29:13 +0000

    Put dashboard in panels

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,16 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <h2>Recent Deliveries</h2>
-            <ul class="list-group">
-                @foreach ($recentDeliveries as $delivery)
-                    <a href="{{ route('deliveries.show', $delivery) }}" class="list-group-item">
-                        <strong>{{ $delivery->user->name }}</strong> delivered <strong>{{ $delivery->cargo->name }}</strong> from <strong>{{ $delivery->from->name }}</strong> to <strong>{{ $delivery->to->name }}</strong> {{ $delivery->created_at->diffForHumans() }}
-                    </a>
-                @endforeach
-            </ul>
+            <div class="panel panel-default">
+                <div class="panel-heading">Recent Deliveries</div>
+                <ul class="list-group">
+                    @foreach ($recentDeliveries as $delivery)
+                        <a class="list-group-item" href="{{ route('deliveries.show', $delivery) }}">
+                            <strong>{{ $delivery->user->name }}</strong> delivered <strong>{{ $delivery->cargo->name }}</strong> from <strong>{{ $delivery->from->name }}</strong> to <strong>{{ $delivery->to->name }}</strong> {{ $delivery->created_at->diffForHumans() }}
+                        </a>
+                    @endforeach
+                </ul>
+            </div>
         </div>
     </div>
 </div>
