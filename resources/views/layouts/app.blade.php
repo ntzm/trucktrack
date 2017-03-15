6de83a903ab2c703b7a21ff65697791acc0071a5
Nat Zimmermann <nat@ntzm.io>
2017-03-15 21:13:38 +0000

    Remove ARIA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,10 +32,10 @@
                         <li><a href="{{ route('register') }}">Register</a></li>
                     @else
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
-                            <ul class="dropdown-menu" role="menu">
+                            <ul class="dropdown-menu">
                                 <li>
                                     <a href="{{ route('users.show', Auth::user()) }}">Profile</a>
                                 </li>
