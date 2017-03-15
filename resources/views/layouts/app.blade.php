cbe19d9392ab20ba227c6fe7db428234d92f00ae
Nat Zimmermann <nat@ntzm.io>
2017-03-15 21:14:15 +0000

    Flip if

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,10 +27,7 @@
                     </ul>
                 @endif
                 <ul class="nav navbar-nav navbar-right">
-                    @if (Auth::guest())
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
-                    @else
+                    @if (Auth::check())
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                                 {{ Auth::user()->name }} <span class="caret"></span>
@@ -50,6 +47,9 @@
                                 </li>
                             </ul>
                         </li>
+                    @else
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
                     @endif
                 </ul>
             </div>
